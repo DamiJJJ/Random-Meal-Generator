35325d299a7f04826140f12a764078b6b8f6e6e6
DamiJJJ <damipl1280@outlook.com>
2022-11-16 13:26:02 +0100

    printMeal function with image display

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,27 @@
 const button = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
 
 const generateMeal = () => {
 
     const URL = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
     axios.get(URL).then(response => {
-        console.log(response.data)     
+        printMeal(response.data.meals[0])     
     })
     .catch(error => {
         error => console.error(error)
     })   
 }
+
+const printMeal = (meal) => {
+    console.log(meal)
+
+    const newInnerHTML = `
+    <div class="imgContainer">
+        <img src="${meal.strMealThumb}" alt="Meal Image">
+    </div>`;
+
+    meal_container.innerHTML = newInnerHTML;
+}
+
 button.addEventListener('click', generateMeal)
